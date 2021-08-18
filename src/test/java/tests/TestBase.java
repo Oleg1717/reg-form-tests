3afb2ffb30a0f90ecec9ce9ea5f57a42fe9a0f63
Oleg1717 <olekmail@yandex.ru>
2021-08-18 12:53:14 +0300

    small correction

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,8 +1,12 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.Step;
+import io.qameta.allure.junit5.AllureJunit5;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static config.Project.isVideoOn;
@@ -10,9 +14,11 @@ import static helpers.AllureAttachments.*;
 import static helpers.DriverSettings.configure;
 import static helpers.DriverUtils.getSessionId;
 
+@ExtendWith({AllureJunit5.class})
 public class TestBase {
     @BeforeAll
     static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         configure();
     }
 
